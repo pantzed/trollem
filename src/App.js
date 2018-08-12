22b759015ab5b144bbbdc91221ef4a42d948b59d
edpantzar@gmail.com <edpantzar@gmail.com>
2018-08-12 18:03:05 -0500

    Can now render a list of posts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const postData = {
+const posts= [
+  {
+    previewText: `
+      Bacon ipsum dolor amet cow tongue prosciutto meatloaf, 
+      boudin meatball biltong tail andouille leberkas cupim 
+      bresaola spare ribs hamburger short loin.`,
+    title: `Star Gazing`,
+    author: `Troller #1`,
+    imgSrc: `https://images.pexels.com/photos/33688/delicate-arch-night-stars-landscape.jpg?auto=compress&cs=tinysrgb&dpr=2&h=350`,
+    imgAlt: `Moab arch and night sky`,
+  },
+  {
   previewText: `
     Bacon ipsum dolor amet cow tongue prosciutto meatloaf, 
     boudin meatball biltong tail andouille leberkas cupim 
     bresaola spare ribs hamburger short loin.`,
-  title: `Star Gazing`,
-  author: `Troller #1`,
-  imgSrc: `https://images.pexels.com/photos/33688/delicate-arch-night-stars-landscape.jpg?auto=compress&cs=tinysrgb&dpr=2&h=350`,
-  imgAlt: `Moab arch and night sky`,
-}
+  title: `Markers and Shit`,
+  author: `Massive Troll`,
+  imgSrc: `https://images.pexels.com/photos/1152665/pexels-photo-1152665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350`,
+  imgAlt: `Colored markers`,
+  },
+];
 
 function Image(props) {
   return (
-    <img className="img-fluid rounded p-3" src={props.imgSrc} alt={props.imgAlt} />
+    <img className="img-fluid rounded p-3" src={props.imgSrc} alt={props.imgAlt}/>
   );
 }
 
@@ -48,25 +60,38 @@ function Preview(props) {
   );
 }
 
-function Post(props) {
-  return (
-    <div className="row mt-5 bg-light border rounded">
-      <div className="col-xs-12 col-sm-4 col-lg-3">
-        <Image imgSrc={props.post.imgSrc} imgAlt={props.post.imgAlt} />
+class Post extends React.Component {
+  renderPost(post, index) {
+    return (
+      <div className="row mt-5 bg-light border rounded" key={index}>
+        <div className="col-xs-12 col-sm-4 col-lg-3">
+          <Image imgSrc={post.imgSrc}/>
+        </div>
+        <div className="col-xs-12 col-sm-6 col-lg-7 p-3">
+          <Title title={post.title}/>
+          <Preview previewText={post.previewText}/>
+          <span> 1 Day ago | 0 comments </span>
+          <Comment comment=">>> this is a comment!" />
+        </div>
+        <div className="col-xs-12 col-sm-2 p-3 text-right">
+          <Author author={post.author}/>
+        </div>
       </div>
-      <div className="col-xs-12 col-sm-6 col-lg-7 p-3">
-        <Title title={props.post.title} />
-        <Preview previewText={props.post.previewText} />
-        <span> 1 Day ago | 0 comments </span>
-        <Comment comment=">>> this is a comment!" />
+    );
+  }
+  render() {
+    const postList = posts.map((post, index) => {
+      return this.renderPost(post, index);
+    });
+    return (
+      <div>
+        {postList}
       </div>
-      <div className="col-xs-12 col-sm-2 p-3 text-right">
-        <Author author={props.post.author} />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 class App extends Component {
+  
   render() {
     return (
       <div>
@@ -76,7 +101,7 @@ class App extends Component {
         <div className="container-fluid">
           <div className="row d-flex justify-content-center">
             <div className="col-11">
-              <Post post={postData}/>
+              <Post posts={posts}/>
             </div>
           </div>
         </div>
